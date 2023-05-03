60626ed204b4eca71a26fccd6be250ad1349ef1c
EshraqEZ <131262785+EshraqEZ@users.noreply.github.com>
2023-05-03 13:27:45 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,26 +9,43 @@ radio.onReceivedNumber(function (receivedNumber) {
             `)
         music.playTone(330, music.beat(BeatFraction.Whole))
         basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    }
+    if (receivedNumber == 0) {
+        basic.showLeds(`
             . . . . .
             . . . . .
             . . . . .
             . . . . .
             . . . . .
             `)
-        music.playTone(330, music.beat(BeatFraction.Whole))
     }
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(2)
 })
-input.onGesture(Gesture.ScreenUp, function () {
+input.onButtonPressed(Button.AB, function () {
     music.setVolume(255)
-})
-input.onGesture(Gesture.ScreenDown, function () {
-    music.setVolume(0)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(3)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.setVolume(0)
 })
 basic.forever(function () {
     radio.setGroup(69)
